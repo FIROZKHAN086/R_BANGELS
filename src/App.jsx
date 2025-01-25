a3b67a426c2afa3f5f6a38e2e599957cb344f122
FIROZ KHAN <computerpc096.com>
2025-01-25 13:06:37 -0800

    folder str

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Main from './Component/Main'
+import Cart from './Pages/Cart'
+import Login from './Pages/Login'
+import Navbar from './Component/Navbar'
+import Foter from './Component/Foter'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+  <Routes>
+ <Route path='/' element={<Main/>} />
+ <Route path="/cart" element={<Cart/>} />
+ <Route path="/login" element={<Login/>}/>
+</Routes>
+<Foter/>
+    </div>
   )
 }
 
---
+++ src/Context/Context.jsx
@@ -0,0 +1,18 @@
+import React ,{createContext} from "react";
+
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+return (
+    <StoreContext.Provider  >
+            {props.children}
+        </StoreContext.Provider>
+)
+
+
+}
+
+
+export default StoreContextProvider;
