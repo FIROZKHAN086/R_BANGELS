8a9cd771c0e011804e523e9a1f98db9c5009bbf9
FIROZ KHAN <computerpc096@gmail.com>
2025-02-06 20:02:12 -0800

    ok

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -18,8 +18,8 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+    <div className="flex  items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full shadow-slate-950 shadow-xl max-w-md p-8 bg-white rounded-lg ">
         {/* Heading */}
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
 
--- src/Pages/Singup.jsx
+++ src/Pages/Singup.jsx
@@ -21,7 +21,7 @@ const Singup = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-slate-950 shadow-xl">
         {/* Heading */}
         <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
 
